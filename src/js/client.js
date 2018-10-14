3ced0b729e86c2be36fdfec6d1daba26a1021e6a
AMutaa <piusandru@gmail.com>
2018-10-14 18:39:49 -0400

    update

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,13 @@
 import { createStore } from 'redux'
 
-const reducer = function () {
-
+const reducer = function (state, action) {
+  if (action.type === 'INC') {
+    return state + action.payload;
+  }
+  if (action.type === 'DEC') {
+    return state - action.payload;
+  }
+  return state
 }
 
 const store = createStore(reducer, 0)
@@ -9,3 +15,9 @@ const store = createStore(reducer, 0)
 store.subscribe(() => {
   console.log('store changed', store.getState())
 })
+
+store.dispatch({ type: 'INC', payload: 1 })
+store.dispatch({ type: 'INC', payload: 2 })
+store.dispatch({ type: 'INC', payload: 22 })
+store.dispatch({ type: 'INC', payload: 1 })
+store.dispatch({ type: 'DEC', payload: 1000 })
