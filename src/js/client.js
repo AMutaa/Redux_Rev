544d7dfd0c3c48334a0f0decb6f248f2d29dfc2c
AMutaa <piusandru@gmail.com>
2018-10-15 17:11:13 -0400

    redux with async and promises

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
 import axios from 'axios';
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+
 
 const initialState = {
   fetching: false,
@@ -13,14 +15,14 @@ const initialState = {
 
 const reducer = (state = {}, action) => {
   switch (action.type) {
-    case "FETCH_MOVIES_START": {
+    case "FETCH_MOVIES_PENDING": {
       return {
         ...state,
         fetching: true
       }
       break;
     }
-    case "RECEIVE_MOVIES": {
+    case "RECEIVE_MOVIES_REJECTED": {
       return {
         ...state,
         fetching: false,
@@ -28,7 +30,7 @@ const reducer = (state = {}, action) => {
       }
       break;
     }
-    case "RECEIVE_MOVIES": {
+    case "RECEIVE_MOVIES_FULFILLED": {
       return {
         ...state,
         fetching: false,
@@ -41,20 +43,15 @@ const reducer = (state = {}, action) => {
   return state
 }
 
-const middleware = applyMiddleware(thunk, logger())
+const middleware = applyMiddleware(promise(), thunk, logger())
 const store = createStore(reducer, middleware)
 
 store.subscribe(() => {
   console.log("store changed", store.getState())
 })
 
-store.dispatch((dispatch) => {
-  dispatch({ type: "FETCH_USERS_START" })
-  axios.get('https://api.themoviedb.org/3/discover/movie?api_key=131c87cb54773ae0f6c9b813ce9041b2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
-    .then((response) => {
-      dispatch({ type: "RECEIVE_MOVIES", payload: response.data })
-    })
-    .catch((err) => {
-      dispatch({ type: 'FETCH_MOVIES_ERROR', payload: err })
-    })
+store.dispatch({
+  type: "FETCH_MOVIES",
+  payload: axios.get('https://api.themoviedb.org/3/discover/movie?api_key=131c87cb54773ae0f6c9b813ce9041b2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+
 })
